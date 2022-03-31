71181d02770fe79351e600e1cc4eb529ef075612
Tarun800 <62602349+Tarun800@users.noreply.github.com>
2022-03-31 09:16:28 +0530

    Create records.h

---
+++ 3_Implementation/Inc/records.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void add();
+	void password();
+	void displayAll();
+	void record();
+	void del();
+	void marksheet();
+	void modify();
+	void changePassword();
+
+##end if
